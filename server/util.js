e9f93f920abe552fe4a7805461fc4d062d533d54
AnDev <57936145+AnDev77@users.noreply.github.com>
2024-11-27 13:21:54 +0900

    util.js 수정

--- server/util.js
+++ server/util.js
@@ -1,14 +1,14 @@
 const getNextDate= (nowDate)=>{
-    let temp = nowDate.split(" ");
-    nowDate = new Date(temp[0]);
-    nowDate.setDate(nowDate.getDate() + 1);
-    let year = nowDate.getFullYear();
-    let month = ('0' + (nowDate.getMonth() + 1)).slice(-2);
-    let day = ('0' + nowDate.getDate()).slice(-2);
+    let temp = new Date(nowDate);
+    temp.setDate(temp.getDate() + 1);
+    let year = temp.getFullYear();
+    let month = ('0' + (temp.getMonth() + 1)).slice(-2);
+    let day = ('0' + temp.getDate()).slice(-2);
 
     let dateString = year + '-' + month  + '-' + day;
-
     dateString += " 00:00:00";
     return dateString;
 }
+
+
 module.exports = getNextDate;
